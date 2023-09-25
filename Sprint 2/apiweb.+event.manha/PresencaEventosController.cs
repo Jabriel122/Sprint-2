f9dbe157184d7444c5f31e86248c680718e2ff15
Gabriel March <34798408+Jabriel122@users.noreply.github.com>
2023-09-25 08:02:06 -0300

    Infelizmente só faltou Presença Eventos

---
+++ Sprint 2/apiweb.+event.manha/PresencaEventosController.cs
@@ -0,0 +1,20 @@
+﻿using apiweb._event.manha.Interface;
+using apiweb._event.manha.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiweb._event.manha.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PresencaEventosController : ControllerBase
+    {
+
+        private readonly IPresencaEventoRepository _presecnasEventosRepository;
+
+        public PresencaEventosController()
+        {
+            _presecnasEventosRepository = new PresencaEventoRepository();
+        }
+    }
+}
